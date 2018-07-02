a39fb377c5c2b7e1074e3098239c464e4cf5822a
Roy Hopkins <rhopkins@MacBookPros-MacBook-Pro.local>
2018-07-02 23:33:30 +0100

    Tidy up styles and use flexbox to arrange faders

--- src/app/components/fader/fader.component.ts
+++ src/app/components/fader/fader.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'fader',
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaderComponent implements OnInit {
 
+  // Title of fader. This also is the text that is displayed on the button
+  @Input()
+  name : string;
+
+  // Event that is generated when the title is clicked
+  @Output()
+  clickTitle = new EventEmitter();
+
+  // Current slider value
+  value: number = 0;
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // Event handler for when the slider value changes
+  setValue(value : number) {
+    this.value = 1.0 - value;
+  }
+
+  // Event handler for when the title is clicked
+  onClick(button) {
+    this.clickTitle.emit();
   }
 
 }
